bddfda15da252636b95b4d0679844211dfbe2775
Bas van der Hoek <bas@hoek.com>
2015-01-18 14:34:46 +0100

    Initial Commit

---
+++ Auto Store/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Auto Store
+//
+//  Created by Bas van der Hoek on 18/01/15.
+//  Copyright (c) 2015 Bas van der Hoek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
