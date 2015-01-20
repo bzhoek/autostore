64e33aa9d5fc0d8a3c3a809e47f1377b4313a818
Bas van der Hoek <bvdhoek@zilverline.com>
2015-01-20 13:41:19 +0100

    copied tests from other auto store project

--- Auto StoreTests/Auto_StoreTests.swift
+++ Auto StoreTests/Auto_StoreTests.swift
@@ -1,10 +1,26 @@
 import Cocoa
 import XCTest
 
+public extension AXUIElement {
+  public func getAttribute<T>(property: String) -> T? {
+    var ptr: Unmanaged<AnyObject>?
+    if AXUIElementCopyAttributeValue(self, property, &ptr) != AXError(kAXErrorSuccess) {
+      return nil
+    }
+    return ptr.map {
+      $0.takeRetainedValue() as T
+    }
+  }
+}
+
+enum AXAttributes: String {
+  case Description = "AXDescription", Role = "AXRole", Windows = "AXWindows"
+}
+
 class Auto_StoreTests: XCTestCase {
 
   // https://github.com/sdegutis/DIY-Window-Manager/blob/master/Desktop/Desktop/App.swift
-  func testRuanningApplication() {
+  func testRunningApplication() {
     let appstore: NSRunningApplication? = NSRunningApplication.runningApplicationsWithBundleIdentifier("com.apple.appstore").first as? NSRunningApplication
     let pid = appstore?.processIdentifier
     let app = AXUIElementCreateApplication(pid!).takeRetainedValue()
@@ -18,5 +34,40 @@ class Auto_StoreTests: XCTestCase {
     let accessEnabled = AXIsProcessTrustedWithOptions(privOptions)
     XCTAssert(accessEnabled == 1, "Accessibility for Xcode not enabled")
   }
+
+  func attributeValue(element: AXUIElement, attribute: AXAttributes) -> String? {
+    var ptr: Unmanaged<AnyObject>?
+    if AXUIElementCopyAttributeValue(element, attribute.rawValue, &ptr) != AXError(kAXErrorSuccess) {
+      return nil
+    }
+    return ptr?.takeRetainedValue() as? String
+  }
+
+//  func testExample() {
+//    for win in windows() {
+//      let name = win[kCGWindowOwnerName as NSString]
+//      if name as NSString == "App Store" {
+//        let pidi = win[kCGWindowOwnerPID as NSString] as NSInteger
+//        let pid: pid_t = Int32(pidi)
+//        let app = AXUIElementCreateApplication(pid).takeUnretainedValue()
+//        var ptr: Unmanaged<AnyObject>?
+//        AXAttributes.Windows.rawValue
+//        AXUIElementCopyAttributeValue(app, AXAttributes.Windows.rawValue, &ptr)
+//        let windowList: AnyObject? = ptr?.takeRetainedValue()
+//        println(windowList)
+//        println(CFArrayGetCount(windowList as CFArray))
+//        let windowRef = CFArrayGetValueAtIndex(windowList as CFArray, 0)
+//        println(windowRef)
+//        AXUIElementCopyAttributeValue(app, "AXMainWindow", &ptr)
+//        let element = ptr?.takeRetainedValue() as AXUIElementRef
+//        let description = CFCopyTypeIDDescription(CFGetTypeID(element))
+//        println("type = \(description)")
+//        println(element)
+//        println(self.attributeValue(element, attribute: .Role))
+//        println(self.attributeValue(element, attribute: .Description))
+//      }
+//    }
+//  }
+
 
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+http://stackoverflow.com/questions/21069066/move-other-windows-on-mac-os-x-using-accessibility-api
+https://developer.apple.com/library/ios/documentation/Swift/Conceptual/BuildingCocoaApps/InteractingWithCAPIs.html
+https://github.com/sdegutis/DIY-Window-Manager/tree/master/Desktop/Desktop
